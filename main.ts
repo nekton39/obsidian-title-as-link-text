48d92171f8d3c87ad48d0966e993c0dafc5771f5
nekton39 <sunmoonktkr@gmail.com>
2023-09-30 18:10:27 +0900

    Fixed to use decodeURICompopnent

--- main.ts
+++ main.ts
@@ -92,8 +92,8 @@ export default class TitleAsLinkTextPlugin extends Plugin {
       const newFileContent = fileContent.replace(
         /\[(.*?)\]\(<?([^>]+)>?\)/g,
         (_, linkText, linkUrl) => {
-          const linkUrlDecoded = linkUrl
-          // const linkUrlDecoded = decodeURIComponent(linkUrl);
+          //const linkUrlDecoded = linkUrl
+          const linkUrlDecoded = decodeURIComponent(linkUrl);
           if (basename(linkUrlDecoded) === basename(oldPath)) {
             return `[${title}](<${linkUrl}>)`;
           }
